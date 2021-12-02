f9ecdb4e637873ebbaed6ddbe6d52eebb743f66b
Jamie <jamienolan93@hotmail.co.uk>
2021-12-02 14:48:38 +0000

    MVP Complete

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,22 @@
+from werkzeug.wrappers import request
 from app import app
-from flask import render_template
-from models.events_list import events
+from flask import render_template, request
+from models.event import Event
+from models.events_list import *
 
 @app.route('/events')
 def index():
     return render_template('index.html', title="Home", events=events)
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    event_name = request.form['name']
+    event_disc = request.form['description']
+    event_date = request.form['date']
+    guest_count = request.form['guest_count']
+    location = request.form['location']
+    new_event = Event(event_date, event_name, guest_count, location, event_disc)
+    add_new_event(new_event)
+    return render_template('index.html', title="Home", events=events)
+
+
--- models/events_list.py
+++ models/events_list.py
@@ -3,3 +3,6 @@ from models.event import *
 event1 = Event("25th December", "Christmas Dinner", 1, "The Chanter", "Meal for one")
 event2 = Event("17th December", "End of Term Party", 16, "The Stand Up", "Fun with friends")
 events = [event1, event2]
+
+def add_new_event(event):
+    events.append(event)
